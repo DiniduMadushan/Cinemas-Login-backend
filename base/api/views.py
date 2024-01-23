66b294e54d9216c4cd2f53f1f110e3d4072c0379
DiniduMadushan <dinidumadushan15576@gmail.com>
2024-01-23 09:43:08 +0530

    first commit

---
+++ base/api/views.py
@@ -0,0 +1,14 @@
+from django.http import JsonResponse
+
+def getRoutes(request):
+    
+    routes=[
+        '/api/token',
+        '/api/token,refresh'
+    ]
+    
+    return JsonResponse(routes,safe=False)
+    
+     
+
+  
