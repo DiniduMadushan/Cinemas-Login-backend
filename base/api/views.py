97fdbe7ed2aed623ccce39da3eb9006b88134d4b
DiniduMadushan <dinidumadushan15576@gmail.com>
2024-01-25 13:03:13 +0530

    backend completed

--- base/api/views.py
+++ base/api/views.py
@@ -1,9 +1,13 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import NoteSerializer
+from base.models import Note
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -29,6 +33,16 @@ def getRoutes(request):
     ]
     
     return Response(routes)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):
+    user=request.user,
+    notes = user.note_set.all(),
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
     
      
 
